b52607b507c9985ee1d84d401f4d83dadbc2f79a
Masaya Myojin <m.myojin0425@gmail.com>
2016-07-26 15:58:30 +0900

    tweak

--- app/models/room.rb
+++ app/models/room.rb
@@ -25,7 +25,7 @@ class Room < ApplicationRecord
     )
     gc.update!(
       channel_id: channel.id,
-      expired_at: Time.zone.at(channel.expiration.to_i / 1000),
+      expired_at: Time.zone.at(channel.expiration.to_i / 1000.0),
     )
   end
 
